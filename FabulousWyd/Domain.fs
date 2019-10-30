9669f539f7dcee1fcd06a9530e69ff087dfb8bbc
ByBlener <blener@hotmail.com>
2019-10-30 13:03:17 -0300

    Tudo funcionando (só ta feio mesmo)

---
+++ FabulousWyd/Domain.fs
@@ -0,0 +1,146 @@
+module Domain
+
+open System.Diagnostics
+open FSharp.Data
+open LiteDB
+open LiteDB.FSharp
+open Newtonsoft.Json
+
+let ApiAll = "https://wydmisc.raidhut.com.br/droplist/global-br/data/all.json"
+let ApiAllMaps = "https://wydmisc.raidhut.com.br/droplist/global-br/data/map.json"
+let ApiType type' index = sprintf "https://wydmisc.raidhut.com.br/droplist/global-br/data/%s/%s.json" type' index
+
+type IndexId = int
+type ItemId = int
+type MobId = int
+type MapId = int
+type MapName = string
+type MobName = string
+
+[<CLIMutable>]
+type Index =
+    { Id: IndexId
+      Type: string
+      Name: string }
+
+[<CLIMutable>]
+type Item =
+    { Id: ItemId
+      Index: int
+      Name: string
+      Rarity: string }
+    
+[<CLIMutable>]
+type Mob =
+    { Id: MobId
+      Name: MobName
+      Level: int
+      Hp: int
+      Mp: int
+      Exp: int
+      Maps: Index list
+      Drops: Item list }
+
+[<CLIMutable>]
+type Map =
+    { Id: MapId
+      Name: MapName
+      Mobs: Index list }
+
+type Msg =
+    | IndexLoaded of Index list
+    | MapsLoaded of Map list
+    | MobsLoaded of Mob list
+    
+    | ReloadIndexes
+    | ReloadMapas
+    | ReloadMobs
+    
+type Cmd =
+    | LoadIndex
+    | LoadMaps of MapName list
+    | LoadMobs of MobName list
+    
+let fetchFromServer url =
+    Http.AsyncRequestString(url)
+    
+let GetCollection<'a> (sqlPath:string) =
+    let mapper = FSharpBsonMapper()
+    use db = new LiteDatabase(sqlPath, mapper)
+    db.GetCollection<'a>()
+        
+let Upsert<'a> (sqlPath:string) (item:'a) =
+    let collection = GetCollection<'a> sqlPath
+    collection.Upsert item
+    
+let InsertAll<'a> (sqlPath:string) (items: 'a list) =
+    let collection = GetCollection<'a> sqlPath
+    collection.InsertBulk items
+
+let GetAll<'a> (sqlPath:string) =
+    let collection = GetCollection<'a> sqlPath
+    collection.FindAll()
+    
+let GetOneMaybe<'a> (sqlPath:string) (id:string) =
+    let collection = GetCollection<'a> sqlPath
+    let bsonValue = BsonValue id
+    let obj = collection.FindById bsonValue
+    match box obj with
+    | null -> None
+    | _ -> Some obj
+    
+module Index =
+    let LoadIndex (sqlPath:string) : Async<Msg> =
+        async {
+            do! Async.SwitchToThreadPool()
+            let dbIndex = GetAll<Index> sqlPath
+            match dbIndex |> Seq.isEmpty with
+            | false -> return dbIndex |> List.ofSeq |> IndexLoaded
+            | true ->
+                let! serverJson = fetchFromServer ApiAll
+                let indexes = JsonConvert.DeserializeObject<Index list> serverJson
+                do InsertAll sqlPath indexes |> ignore
+                return IndexLoaded indexes
+        }
+        
+module Mob =
+    let LoadMob (mob:string) : Async<Mob> =
+        async {
+            do! Async.SwitchToThreadPool()
+            Debug.WriteLine(sprintf "Loading mob %s" mob)
+            let! serverJson = ApiType "mob" mob |> fetchFromServer
+            return JsonConvert.DeserializeObject<Mob> serverJson
+        }
+        
+    let LoadMobs (sqlPath:string) (mobs: string list) : Async<Msg> =
+        async {
+            do! Async.SwitchToThreadPool()
+            let dbMobs = GetAll<Mob> sqlPath
+            match dbMobs |> Seq.isEmpty with
+            | false -> return dbMobs |> List.ofSeq |> MobsLoaded
+            | true ->
+                let mobTasks = mobs |> List.map LoadMob |> List.map Async.RunSynchronously
+                do InsertAll sqlPath mobTasks |> ignore
+                return MobsLoaded mobTasks
+        }
+    
+module Map =
+    let LoadMap (map:string) : Async<Map> =
+        async {
+            do! Async.SwitchToThreadPool()
+            let! serverJson = ApiType "map" map |> fetchFromServer
+            return JsonConvert.DeserializeObject<Map> serverJson
+        }
+        
+    let LoadMaps (sqlPath:string) (maps: string list) : Async<Msg> =
+        async {
+            do! Async.SwitchToThreadPool()
+            let dbMaps = GetAll<Map> sqlPath
+            match dbMaps |> Seq.isEmpty with
+            | false -> return dbMaps |> List.ofSeq |> MapsLoaded
+            | true ->
+                let mapTasks = maps |> List.map LoadMap
+                let fullMaps = mapTasks |> Async.Parallel |> Async.RunSynchronously |> List.ofArray
+                do InsertAll sqlPath fullMaps |> ignore
+                return MapsLoaded fullMaps
+        }
---
+++ FabulousWyd/FabulousWyd.fs
@@ -0,0 +1,186 @@
+﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
+namespace FabulousWyd
+
+open Domain
+
+open FSharp.Data.Runtime
+open System.Diagnostics
+open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+
+module App =
+    let shellRef = ViewRef<Shell>()
+    
+    type Model = 
+      { Index: Index list
+        Maps: Map list
+        Mobs: Mob list }
+
+    let mapCmdToMsg sqlPath cmd =
+        match cmd with
+        | LoadIndex -> Index.LoadIndex sqlPath |> Cmd.ofAsyncMsg
+        | LoadMaps maps -> Map.LoadMaps sqlPath maps |> Cmd.ofAsyncMsg
+        | LoadMobs mobs -> Mob.LoadMobs sqlPath mobs |> Cmd.ofAsyncMsg
+    
+    let initModel = { Index = []; Maps = []; Mobs = [] }
+
+    let init () = initModel, [ LoadIndex ]
+
+    let update msg (model: Model) =
+        match msg with
+        | IndexLoaded indexes ->
+            { model with Index = indexes }, [ LoadMaps (indexes |> List.filter (fun x -> x.Type = "map") |> List.map (fun x -> x.Name)) ]
+        | MapsLoaded maps ->
+            { model with Maps = maps }, [LoadMobs (model.Index |> List.filter (fun x -> x.Type = "mob") |> List.map (fun x -> x.Name))]
+        | MobsLoaded mobs ->
+            { model with Mobs = mobs }, []
+        
+        | ReloadIndexes -> { model with Index = [] }, [ LoadIndex ]
+        | ReloadMapas -> { model with Maps = [] }, [ LoadMaps (model.Index |> List.filter (fun x -> x.Type = "map") |> List.map (fun x -> x.Name)) ]
+        | ReloadMobs -> { model with Mobs = [] }, [ LoadMobs (model.Index |> List.filter (fun x -> x.Type = "mob") |> List.map (fun x -> x.Name)) ]
+
+    let view (model: Model) dispatch =
+        Shell.shell [
+            Shell.Ref shellRef
+            Shell.Items [
+                yield FlyoutItem.flyoutItem [
+                    FlyoutItem.Title "Página Inicial"
+                    FlyoutItem.Items [
+                        Tab.tab [
+                            Tab.Title "Inicio"
+                            Tab.Items [
+                                ShellContent.shellContent [
+                                    ShellContent.Content <|
+                                        ContentPage.contentPage [
+                                            ContentPage.Content <|
+                                                Grid.grid [
+                                                    Grid.Rows [ GridLength.Auto; GridLength.Auto ]
+                                                    Grid.Columns [ for _ in 1..3 -> GridLength.Auto ]
+                                                    Grid.Children [
+                                                        Label.label [
+                                                            Label.GridRow 0
+                                                            Label.GridColumn 1
+                                                            Label.GridColumnSpan 3
+                                                            Label.Text <| sprintf "Status: Index - %i; Mapas - %i; Mobs - %i" model.Index.Length model.Maps.Length model.Mobs.Length
+                                                        ]
+                                                        Label.label [
+                                                            Label.GridRow 1
+                                                            Label.GridColumn 1
+                                                            Label.GridColumnSpan 3
+                                                            Label.Text <|
+                                                                if model.Index.Length = 0 then "Carregando indexes, por favor aguarde..."
+                                                                elif model.Maps.Length = 0 then "Carregando mapas, por favor aguarde..."
+                                                                elif model.Mobs.Length = 0 then "Carregando mobs, por favor aguarde..."
+                                                                else ""
+                                                        ]
+                                                    ]
+                                                ]
+                                                
+                                        ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+                for i in model.Maps do
+                    yield FlyoutItem.flyoutItem [
+                        FlyoutItem.Title i.Name
+                        FlyoutItem.Items [
+                            Tab.tab [
+                                Tab.Title "Mobs"
+                                Tab.Route <| sprintf "%sMob" i.Name
+                                Tab.Items [
+                                    ShellContent.shellContent [
+                                        ShellContent.Content <|
+                                            ContentPage.contentPage [
+                                                ContentPage.Content <|
+                                                    ScrollView.scrollView [
+                                                        ScrollView.Content <|
+                                                            StackLayout.stackLayout [
+                                                                StackLayout.Children [
+                                                                    for m in i.Mobs do
+                                                                        let mob = model.Mobs |> List.tryFind (fun x -> x.Name = m.Name)
+                                                                        match mob with
+                                                                        | None -> yield ActivityIndicator.activityIndicator [ ActivityIndicator.IsRunning true ]
+                                                                        | Some m ->
+                                                                            if not m.Drops.IsEmpty then
+                                                                                yield Label.label [ Label.Text m.Name ]
+                                                                                for d in m.Drops do
+                                                                                    let raridade =
+                                                                                        match d.Rarity with
+                                                                                        | "veryeasy" -> "Muito Fácil"
+                                                                                        | "easy" -> "Fácil"
+                                                                                        | "medium" -> "Mediano"
+                                                                                        | "hard" -> "Dificil"
+                                                                                        | "rare" -> "Raro"
+                                                                                        | _ -> ""
+                                                                                    yield Label.label [ Label.Text <| sprintf "%s - %s" d.Name raridade; Label.TextColor Color.Green ]
+                                                                ]
+                                                            ]
+                                                    ]
+                                            ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+            ]
+        ]
+
+    // Note, this declaration is needed if you enable LiveUpdate
+    let program mapCmdToMsg = Program.mkProgramWithCmdMsg init update view mapCmdToMsg
+
+type App (sqlPath) as app = 
+    inherit Application ()
+
+    let sqlPath = System.IO.Path.Combine(sqlPath, "FabulousWyd.db")
+    let mapCmdToMsg = App.mapCmdToMsg sqlPath
+    
+    let runner = 
+        App.program mapCmdToMsg
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> XamarinFormsProgram.run app
+
+#if DEBUG
+    // Uncomment this line to enable live update in debug mode. 
+    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/tools.html#live-update for further  instructions.
+    //
+    //do runner.EnableLiveUpdate()
+#endif    
+
+    // Uncomment this code to save the application state to app.Properties using Newtonsoft.Json
+    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/models.html#saving-application-state for further  instructions.
+#if APPSAVE
+    let modelId = "model"
+    override __.OnSleep() = 
+
+        let json = Newtonsoft.Json.JsonConvert.SerializeObject(runner.CurrentModel)
+        Console.WriteLine("OnSleep: saving model into app.Properties, json = {0}", json)
+
+        app.Properties.[modelId] <- json
+
+    override __.OnResume() = 
+        Console.WriteLine "OnResume: checking for model in app.Properties"
+        try 
+            match app.Properties.TryGetValue modelId with
+            | true, (:? string as json) -> 
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, json = {0}", json)
+                let model = Newtonsoft.Json.JsonConvert.DeserializeObject<App.Model>(json)
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, model = {0}", (sprintf "%0A" model))
+                runner.SetCurrentModel (model, Cmd.none)
+
+            | _ -> ()
+        with ex -> 
+            App.program.onError("Error while restoring model found in app.Properties", ex)
+
+    override this.OnStart() = 
+        Console.WriteLine "OnStart: using same logic as OnResume()"
+        this.OnResume()
+#endif
+
+
